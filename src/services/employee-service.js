48d2005a24c9c5c83a7a34ab9e639ec76fa77a51
padmini v <padmini.v@perfios.com>
2021-11-12 21:04:41 +0530

    MODIFIED: Ability to call Addemployee json server API using axios

--- src/services/employee-service.js
+++ src/services/employee-service.js
@@ -1,14 +1,9 @@
-import config from "../config/config";
-import React, { Component } from 'react'
+import config from '../config/config';
+import AxiosService from './axios-service'
 
-const axios=require('axios').default;
-
-export default class EmployeeService{
-
-    baseUrl=config.baseUrl;
-    addEmployee(data)
-    {
-        console.log(data)
-        return axios.post(`${this.baseUrl}employee`,data);
+export default class EmployeeService {
+    baseUrl = config.baseUrl;
+    addEmployee(data) {
+        return AxiosService.postService(`${this.baseUrl}employee`, data);
     }
 }
