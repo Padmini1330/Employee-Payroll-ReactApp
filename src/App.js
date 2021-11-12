ee7d441fd3cdfea7c64823181faf243f1fa07fb3
padmini v <padmini.v@perfios.com>
2021-11-12 21:02:03 +0530

    REFACTORED

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import './App.css';
 import React, { Component }  from 'react';
-import PayrollForm from './components/payroll-form/payroll-form';
+import PayrollForm from './components/payroll-form/payroll-form.jsx';
 import { Route,Routes,BrowserRouter } from 'react-router-dom';
 
 function App() {
   return (
     <BrowserRouter>  
     <Routes>
-      <Route path="/" element={<PayrollForm />} ></Route> 
+      <Route path="/" element={<PayrollForm />}/> 
     </Routes>
     </BrowserRouter>
   );
 }
-
 export default App;
+
