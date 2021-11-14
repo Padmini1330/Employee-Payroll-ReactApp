b2392ddbdea79c59569362dac129f95f7e2a4ee2
padmini v <padmini.v@perfios.com>
2021-11-14 15:21:20 +0530

    ADDED: AxiosService method to perform delete call operation

--- src/services/axios-service.js
+++ src/services/axios-service.js
@@ -9,5 +9,14 @@ class AxiosService{
     {
         return axios.get(url,payload,tokenRequired && httpOptions);
     }
+
+    putService(url='',payload = null,tokenRequired=false,httpOptions=null) 
+    {
+        return axios.put(url, payload,tokenRequired && httpOptions);
+    }
+
+    deleteService(url = '', tokenRequired = false, httpOptions = null) {
+        return axios.delete(url, tokenRequired && httpOptions);
+    }
 }
 module.exports = new AxiosService();
